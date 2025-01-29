9c56f7ee90df9be3d9fa7f973be04480648bb367
raimonizard <raimon.izard@gmail.com>
2025-01-29 19:00:56 +0100

    feat: add Clickable functionallity (work in progress)

--- app/src/main/java/com/example/andoidstudio_recyclerview_demo/view/DetailScreen.kt
+++ app/src/main/java/com/example/andoidstudio_recyclerview_demo/view/DetailScreen.kt
@@ -1,6 +1,23 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.example.andoidstudio_recyclerview_demo.model.Pokemon
+import com.example.andoidstudio_recyclerview_demo.viewmodel.getPokemonList
+
 @Composable
 fun DetailScreen(navController: NavController, pokemonName: String){
-    val pokemon: Pokemon = getPokemonList().find { it.name == pokemonName }
+    var pokemon: Pokemon? = null
+
+    for (p in getPokemonList())
+        if (p.name == pokemonName)
+            pokemon = p
+
     if (pokemon != null){
         Column(Modifier
             .fillMaxSize(),
@@ -10,6 +27,8 @@ fun DetailScreen(navController: NavController, pokemonName: String){
                 contentDescription = pokemon.name,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
+                    .fillMaxSize()
+            )
         }
     }
 }
