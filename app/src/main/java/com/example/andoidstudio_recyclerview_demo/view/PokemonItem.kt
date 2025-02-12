662e0e1c8edd038630e94652b1f29754158095e5
raimonizard <raimon.izard@gmail.com>
2025-02-12 10:46:12 +0100

    chore: favourite icon to the bottom right corner of the Card component

--- app/src/main/java/com/example/andoidstudio_recyclerview_demo/view/PokemonItem.kt
+++ app/src/main/java/com/example/andoidstudio_recyclerview_demo/view/PokemonItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -16,6 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -25,38 +27,42 @@ import com.example.andoidstudio_recyclerview_demo.model.Pokemon
 
 @Composable
 fun PokemonItem(pokemon: Pokemon, onSelectedItem: (String) -> Unit) {
-    Card(border = BorderStroke(2.dp, pokemon.type.color),
+    Card(
+        border = BorderStroke(2.dp, pokemon.type.color),
         modifier = Modifier
             .fillMaxWidth()
-            // Afegir propietat clickable i li definim el comportament
             .clickable { onSelectedItem(pokemon.name) }
     ) {
-        Row {
-            Image(
-                painter = painterResource(id = pokemon.image),
-                contentDescription = pokemon.name,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .padding(10.dp)
-                    .fillMaxHeight()
-                    .background(pokemon.type.color)
-            )
-            Column {
-                Text(
-                    text = pokemon.name,
-                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                    fontWeight = FontWeight.Bold
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = pokemon.image),
+                    contentDescription = pokemon.name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxHeight()
+                        .background(pokemon.type.color)
                 )
-                Text(
-                    text = pokemon.type.name.lowercase().replaceFirstChar { it.uppercase() },
-                    color = pokemon.type.color,
-                )
+                Column {
+                    Text(
+                        text = pokemon.name,
+                        fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = pokemon.type.name.lowercase().replaceFirstChar { it.uppercase() },
+                        color = pokemon.type.color,
+                    )
+                }
             }
             Icon(
                 imageVector = Icons.Filled.Favorite,
                 contentDescription = "Favorite",
                 tint = pokemon.type.color,
-                modifier = Modifier.padding(10.dp)
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.BottomEnd)
             )
         }
     }
